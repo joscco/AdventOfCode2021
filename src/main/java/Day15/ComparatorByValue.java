c9a68f4228a000d26e82c83183777a78feebbeaa
Jonathan Schmitz <jomitz96@hotmail.com>
2021-12-15 21:57:53 +0100

    Speed up Day 15 a bit more

--- src/main/java/Day15/ComparatorByValue.java
+++ src/main/java/Day15/ComparatorByValue.java
@@ -5,19 +5,19 @@ import main.java.Day5.Tuple;
 import java.util.Comparator;
 
 public class ComparatorByValue implements Comparator<Tuple<Integer, Integer>> {
-    private final long[][] arr;
+    private final int[][] arr;
 
-    public ComparatorByValue(long[][] distances) {
+    public ComparatorByValue(int[][] distances) {
         this.arr = distances;
     }
 
 
     @Override
     public int compare(Tuple<Integer, Integer> o1, Tuple<Integer, Integer> o2) {
-        long val1 = arr[o1._0][o1._1];
-        long val2 = arr[o2._0][o2._1];
+        int val1 = arr[o1._0][o1._1];
+        int val2 = arr[o2._0][o2._1];
         if (val1 != val2) {
-            return (int) (val1 - val2);
+            return val1 - val2;
         }
         return o1._0 + o1._1 - o2._0 - o2._1;
     }
--- src/main/java/Day15/Day.java
+++ src/main/java/Day15/Day.java
@@ -4,7 +4,9 @@ import main.java.Common.AbstractDay;
 import main.java.Common.InputUtils;
 import main.java.Day5.Tuple;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Day extends AbstractDay {
 
@@ -18,27 +20,28 @@ public class Day extends AbstractDay {
     }
 
     public void solveDay() {
+
+        Long timeStart = System.currentTimeMillis();
         List<String> lines = InputUtils.readInStringLines(getInputStream());
         height = lines.size();
         width = lines.get(0).length();
         nodes = buildNodes(lines);
         solveFirstQuestion();
         solveSecondQuestion();
+        Long timeEnd = System.currentTimeMillis();
+        System.out.println("Full Programm Time: " + (timeEnd - timeStart) + " ms.");
     }
 
     private int[][] buildNodes(List<String> lines) {
         int[][] nodes = new int[width][height];
         for (int y = 0; y < height; y++) {
-            int[] values = Arrays.stream(lines.get(y).split("")).mapToInt(Integer::parseInt).toArray();
-            for (int x = 0; x < width; x++) {
-                nodes[x][y] = values[x];
-            }
+            nodes[y] = Arrays.stream(lines.get(y).split("")).mapToInt(Integer::parseInt).toArray();
         }
         return nodes;
     }
 
     private void solveFirstQuestion() {
-        long shortestDistance = findShortestBetween(new Tuple<>(0, 0), new Tuple<>(width - 1, height - 1));
+        int shortestDistance = findShortestBetween(new Tuple<>(0, 0), new Tuple<>(width - 1, height - 1));
         System.out.println("Part 1:");
         System.out.println(shortestDistance);
     }
@@ -47,15 +50,15 @@ public class Day extends AbstractDay {
         nodes = enlargeNodes(nodes);
         width *= 5;
         height *= 5;
-        long shortestDistance = findShortestBetween(new Tuple<>(0, 0), new Tuple<>(width - 1, height - 1));
+        int shortestDistance = findShortestBetween(new Tuple<>(0, 0), new Tuple<>(width - 1, height - 1));
         System.out.println("Part 2:");
         System.out.println(shortestDistance);
     }
 
     private int[][] enlargeNodes(int[][] nodes) {
-        int[][] newNodes = new int[5*width][5*height];
-        for (int y = 0; y<height; y++) {
-            for(int x = 0; x<width;x++) {
+        int[][] newNodes = new int[5 * width][5 * height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 for (int i = 0; i < 5; i++) {
                     for (int j = 0; j < 5; j++) {
                         newNodes[i * width + x][j * height + y] = wrap(nodes[x][y] + i + j);
@@ -66,20 +69,21 @@ public class Day extends AbstractDay {
         return newNodes;
     }
 
-    private long findShortestBetween(Tuple<Integer, Integer> start, Tuple<Integer, Integer> end) {
+    private int findShortestBetween(Tuple<Integer, Integer> start, Tuple<Integer, Integer> end) {
 
-        long[][] distances = new long[width][height];
+
+        Long timeStart = System.currentTimeMillis();
+        int[][] distances = new int[width][height];
         ComparatorByValue comparator = new ComparatorByValue(distances);
-        PriorityQueue<Tuple<Integer, Integer>> unvisited = new PriorityQueue<>(width*height, comparator);
-        for (int x= 0; x<width; x++) {
-            for(int y = 0; y<height;y++) {
+        PriorityQueue<Tuple<Integer, Integer>> unvisited = new PriorityQueue<>(width * height, comparator);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 distances[x][y] = Integer.MAX_VALUE;
-                unvisited.add(new Tuple<>(x,y));
+                unvisited.add(new Tuple<>(x, y));
             }
         }
 
-        distances[start._0][start._1] = 0L;
-        Long timeStart = System.currentTimeMillis();
+        distances[start._0][start._1] = 0;
 
         while (!unvisited.isEmpty()) {
             Tuple<Integer, Integer> nearestTuple = unvisited.poll();
@@ -94,33 +98,48 @@ public class Day extends AbstractDay {
         return -1;
     }
 
-    private void updateDistanceViaNeightbours(Tuple<Integer, Integer> nearestTuple, PriorityQueue<Tuple<Integer, Integer>> unvisited, long[][] distances) {
-        long nearestDist = distances[nearestTuple._0][nearestTuple._1];
-        for (Tuple<Integer, Integer> currentVertex : getAdjacents(nearestTuple, width, height)) {
-            long temp = nearestDist + nodes[currentVertex._0][currentVertex._1];
-            if (temp < distances[currentVertex._0][currentVertex._1]) {
-                distances[currentVertex._0][currentVertex._1] =  temp;
+    private void updateDistanceViaNeightbours(Tuple<Integer, Integer> nearestTuple, PriorityQueue<Tuple<Integer, Integer>> unvisited, int[][] distances) {
+        int nearestDist = distances[nearestTuple._0][nearestTuple._1];
+        int nearX = nearestTuple._0;
+        int nearY = nearestTuple._1;
+        Tuple<int[], int[]> adjacents = getAdjacents(nearestTuple, width, height);
+        for (int x : adjacents._0) {
+            int temp = nearestDist + nodes[x][nearY];
+            if (temp < distances[x][nearY]) {
+                distances[x][nearY] = temp;
                 // We can just add the same vertex again with another (higher) priority
-                unvisited.add(currentVertex);
+                unvisited.add(new Tuple<>(x, nearY));
+            }
+        }
+        for (int y : adjacents._1) {
+            int temp = nearestDist + nodes[nearX][y];
+            if (temp < distances[nearX][y]) {
+                distances[nearX][y] = temp;
+                // We can just add the same vertex again with another (higher) priority
+                unvisited.add(new Tuple<>(nearX, y));
             }
         }
     }
 
-    private List<Tuple<Integer, Integer>> getAdjacents(Tuple<Integer, Integer> nearestTuple, int width, int height) {
-        List<Tuple<Integer, Integer>> result = new ArrayList<>();
+    private Tuple<int[], int[]> getAdjacents(Tuple<Integer, Integer> nearestTuple, int width, int height) {
+        Tuple<int[], int[]> result = new Tuple<>();
         int currentX = nearestTuple._0;
         int currentY = nearestTuple._1;
-        if (currentX > 0) {
-            result.add(new Tuple<>(currentX - 1, currentY));
+
+        if (currentX == 0) {
+            result._0 = new int[]{1};
+        } else if (currentX == width - 1) {
+            result._0 = new int[]{width - 2};
+        } else {
+            result._0 = new int[]{currentX - 1, currentX + 1};
         }
-        if (currentX < width - 1) {
-            result.add(new Tuple<>(currentX + 1, currentY));
-        }
-        if (currentY > 0) {
-            result.add(new Tuple<>(currentX, currentY - 1));
-        }
-        if (currentY < height - 1) {
-            result.add(new Tuple<>(currentX, currentY + 1));
+
+        if (currentY == 0) {
+            result._1 = new int[]{1};
+        } else if (currentY == height - 1) {
+            result._1 = new int[]{height - 2};
+        } else {
+            result._1 = new int[]{currentY - 1, currentY + 1};
         }
         return result;
     }
