c43b57d60ea2f750c6bc4ede64f5b364fdfbf3fb
Jonathan Schmitz <jomitz96@hotmail.com>
2021-12-22 16:49:21 +0100

    Add Day 22

---
+++ src/main/java/Day22/Cuboid.java
@@ -0,0 +1,56 @@
+package main.java.Day22;
+
+public class Cuboid {
+    int xMin;
+    int xMax;
+    int yMin;
+    int yMax;
+    int zMin;
+    int zMax;
+    boolean on;
+
+    public Cuboid() {}
+
+    public Cuboid(int xMin, int xMax, int yMin, int yMax, int zMin, int zMax, boolean on) {
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+        this.yMax = yMax;
+        this.zMin = zMin;
+        this.zMax = zMax;
+        this.on = on;
+    }
+
+    public Cuboid buildIntersectionWith(Cuboid cuboid) {
+        if (!this.intersectsWith(cuboid)) {
+            return null;
+        } else {
+            // Just take the real intersection here (so don't slice everything up, that got too complicated)
+            // and add it as a new cuboid with positive or negative weight.
+            return new Cuboid(
+                    Math.max(xMin, cuboid.xMin), Math.min(xMax, cuboid.xMax),
+                    Math.max(yMin, cuboid.yMin), Math.min(yMax, cuboid.yMax),
+                    Math.max(zMin, cuboid.zMin), Math.min(zMax, cuboid.zMax),
+                    !on);
+        }
+    }
+
+    private boolean intersectsWith(Cuboid cuboid) {
+        return xMin <= cuboid.xMax && xMax >= cuboid.xMin
+                && yMin <= cuboid.yMax && yMax >= cuboid.yMin
+                && zMin <= cuboid.zMax && zMax >= cuboid.zMin;
+    }
+
+    public long volume() {
+        // This is the real "trick" here. We use negative volumes to take all calculated intersections into account
+        return (xMax - xMin + 1L) * (yMax - yMin + 1L) * (zMax - zMin + 1L) * (on ? 1 : -1);
+    }
+
+    public long limitedAxisVolume(int min, int max) {
+        Cuboid limitedCuboid = new Cuboid(min, max, min, max, min, max, true);
+        if (intersectsWith(limitedCuboid)) {
+            return (xMax - xMin + 1L) * (yMax - yMin + 1L) * (zMax - zMin + 1L) * (on ? 1 : -1);
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/Day22/Day.java
@@ -0,0 +1,104 @@
+package main.java.Day22;
+
+import main.java.Common.AbstractDay;
+import main.java.Common.InputUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day extends AbstractDay {
+
+    public static void main(String[] args) {
+        Day day = new Day();
+        long startTime = System.currentTimeMillis();
+        day.solveDay();
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time needed: " + (endTime - startTime) + "ms");
+    }
+
+    public void solveDay() {
+        List<String> lines = InputUtils.readInStringLines(getInputStream());
+        List<Cuboid> cuboids = buildCuboids(lines);
+        List<Cuboid> mergedCuboids = mergeCuboids(cuboids);
+        solveFirstQuestion(mergedCuboids);
+        solveSecondQuestion(mergedCuboids);
+    }
+
+    private List<Cuboid> mergeCuboids(List<Cuboid> cuboids) {
+        List<Cuboid> alreadyPlacedCuboids = new ArrayList<>();
+        for (Cuboid newCuboid : cuboids) {
+            List<Cuboid> balancingCuboids = new ArrayList<>();
+
+            if (newCuboid.on) {
+                // Add one weight for each new on cuboid point
+                balancingCuboids.add(newCuboid);
+            }
+
+            // If off-cuboids do not have any intersections with old ones, we won't deal with them.
+            // We will work with weights. On Cuboids have one weight for each point.
+            // Instead of slicing up all cuboids, only add negative weight intersection when turning off points again.
+            for (Cuboid oldCuboid : alreadyPlacedCuboids) {
+
+                // This gives four cases for intersection:
+                // Old on, new on (-> weight=2 for each intersection point):
+                // Add intersection with negative weight to return to weight 0 there
+                // Old on, new off (-> weight=1 for each intersection point):
+                // Add intersection with negative weight to return to weight 0 there
+                // Old off (already cancelling out something), new on (-> weight=0 for each intersection point):
+                // Add intersection with positive weight to return to weight 1 there
+                // Old off (already cancelling out something), new off (-> weight=-1 for each intersection point):
+                // Add intersection with positive weight to return to weight 1 there
+
+                // So only the old value really counts
+
+                Cuboid intersection = oldCuboid.buildIntersectionWith(newCuboid);
+                if (intersection != null) {
+                    balancingCuboids.add(intersection);
+                }
+            }
+
+            alreadyPlacedCuboids.addAll(balancingCuboids);
+        }
+        return alreadyPlacedCuboids;
+    }
+
+    private List<Cuboid> buildCuboids(List<String> lines) {
+        List<Cuboid> result = new ArrayList<>();
+        for (String line : lines) {
+            Cuboid cuboid = new Cuboid();
+            String[] lineArr = line.split(" ");
+            cuboid.on = lineArr[0].equals("on");
+
+            String[] limitArr = lineArr[1].split(",");
+            int[] values = Arrays.stream(limitArr)
+                    .map(str -> str.substring(2))
+                    .map(str -> str.split("\\.\\."))
+                    .flatMap(Stream::of)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            cuboid.xMin = values[0];
+            cuboid.xMax = values[1];
+            cuboid.yMin = values[2];
+            cuboid.yMax = values[3];
+            cuboid.zMin = values[4];
+            cuboid.zMax = values[5];
+
+            result.add(cuboid);
+
+        }
+        return result;
+    }
+
+    private void solveFirstQuestion(List<Cuboid> cuboids) {
+        System.out.println("Part 1:");
+        System.out.println(cuboids.stream().mapToLong(c -> c.limitedAxisVolume(-50, 50)).sum());
+    }
+
+    private void solveSecondQuestion(List<Cuboid> cuboids) {
+        System.out.println("Part 2:");
+        System.out.println(cuboids.stream().mapToLong(Cuboid::volume).sum());
+    }
+
+}
